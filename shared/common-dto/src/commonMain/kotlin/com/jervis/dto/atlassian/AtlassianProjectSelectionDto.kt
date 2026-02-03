08c02328874834e6f8bda917d9df324f2264228e
Jan Damek <jan.damek1@moneta.cz>
2026-02-03 13:31:23 +0100

    fix: remove recreated duplicate Atlassian DTOs

--- shared/common-dto/src/commonMain/kotlin/com/jervis/dto/atlassian/AtlassianProjectSelectionDto.kt
+++
@@ -1,9 +0,0 @@
-package com.jervis.dto.atlassian
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class AtlassianProjectSelectionDto(
-    val clientId: String,
-    val projectKey: String,
-)
